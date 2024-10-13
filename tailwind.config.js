414c525221ccb1d4f341736b3fbd12f91ba10bad
2Carrot10 <eleanorgrutz@gmail.com>
2024-10-13 12:30:33 -0700

    updated docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,21 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-		colors: {
-			"tnbg": "#1a1b26"
-
+			"extend": {
+			colors: {
+				"tnbg": "#1a1b26",
+				"tnfg": "#c0caf5",
+				"tnbgalt": "#c0caf5",
+				"tnbgalthigh": "#9aa5ce",
+				"tnhighlight" : "#5a3e8e",
+				"tnblue" : "#c0caf5"
+			},
+			boxShadow: {
+        'w': '0 0px 5px 1px rgba(255, 255, 255, .8)',
+      },
+      backgroundImage: {
+        'cross-pattern': "url(../images/background.svg)"
+      }
 		},
   },
   plugins: [],
