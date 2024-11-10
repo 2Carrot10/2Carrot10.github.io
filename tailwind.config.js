0cb7b41978ac6d9ce7202985c593e895fb3d294f
2Carrot10 <eleanorgrutz@gmail.com>
2024-11-09 16:30:49 -0800

    working but text not done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,11 @@ module.exports = {
 			colors: {
 				"tnbg": "#1a1b26",
 				"tnfg": "#c0caf5",
-				"tnbgalt": "#c0caf5",
+				"tnbgalt": "#414868",
 				"tnbgalthigh": "#9aa5ce",
 				"tnhighlight" : "#5a3e8e",
-				"tnblue" : "#c0caf5"
+				// "tnblue" : "#c0caf5"
+				"tnblue" : "#2ac3de"
 			},
 			boxShadow: {
         'w': '0 0px 5px 1px rgba(255, 255, 255, .8)',
